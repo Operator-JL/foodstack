2e580afe1b8966e151661bf905565202e55f4589
Jesus Lizarraga <jelizarraga@femsafab.com>
2026-03-25 19:26:36 -0700

    Firts files

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, redirect, url_for
+import os
+
+app = Flask(__name__)
+
+app.secret_key = "secret_key"
+
+if __name__ == '__main__':
+   app.run(debug=True)
