8151861339afb5f4b61907eb1ca8080dec536976
Jesús Martín Gallardo <jesusmg@itstepuniv.onmicrosoft.com>
2026-04-08 19:27:04 -0700

    backend user

--- application.py
+++ application.py
@@ -1,10 +1,16 @@
 from flask import Flask, jsonify
+from backend.Controllers.UserController import user_bp
 
 app = Flask(__name__)
+
+# Register Blueprint
+app.register_blueprint(user_bp, url_prefix="/api")
+
 
 @app.route("/")
 def home():
     return "FoodStack API Running"
+
 
 @app.route("/api/test")
 def test():
@@ -12,3 +18,7 @@ def test():
         "status": "ok",
         "message": "API funcionando"
     })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
