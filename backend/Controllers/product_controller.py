c6eef8e17e89c581eeb250f2ffa632431f5eb83c
Krackenslung <quechumartin@hotmail.com>
2026-04-17 22:35:26 -0700

    ADDED USER AND PRODUCT UPDATE OPTION AND PRUDUCT JOIN CATEGORY

--- backend/Controllers/product_controller.py
+++ backend/Controllers/product_controller.py
@@ -3,6 +3,8 @@ import json
 
 from backend.Models.Product import Product
 from backend.Models.category import Category
+from backend.Models.Ingredient import Ingredient
+from backend.Infrastructure.SQLServerConnection import get_connection
 from ..Security.Auth import require_auth
 
 product_bp = Blueprint('product_bp', __name__)
@@ -28,14 +30,37 @@ def get_product_by_id(product_id):
         p = Product(product_id)
 
         category = Category(p.category_id)
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            SELECT i.id, i.name, i.extra_price, ip.default_ingredients
+            FROM ingredients i
+            INNER JOIN ingredients_products ip 
+                ON i.id = ip.ingredient_id
+            WHERE ip.product_id = ?
+            AND ip.status = 1
+        """, (product_id,))
+
+        ingredients = []
+        for row in cursor.fetchall():
+            ingredients.append({
+                "id": row[0],
+                "name": row[1],
+                "extra_price": float(row[2]),
+                "is_default": bool(row[3])
+            })
 
         data = json.loads(p.to_json())
+
         data["category"] = json.loads(category.to_json())
+        data["ingredients"] = ingredients
 
         return jsonify({
             "status": 0,
             "data": data
         })
+
     except Exception as e:
         return jsonify({
             "status": 1,
